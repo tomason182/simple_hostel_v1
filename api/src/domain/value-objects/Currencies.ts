74b219feede4cf02990cd995ea284381d03bed20
tomas <tomas@debian.tomas>
2026-06-26 19:51:11 -0300

    add: Se agrego el value object Currencies

---
+++ api/src/domain/value-objects/Currencies.ts
@@ -0,0 +1,9 @@
+export class Currencies {
+  constructor(
+    public baseCurrency: string,
+    public paymentCurrency: string
+  ) {
+    this.baseCurrency = baseCurrency;
+    this.paymentCurrency = paymentCurrency;
+  };
+}
