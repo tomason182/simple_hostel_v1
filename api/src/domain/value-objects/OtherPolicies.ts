b75b981c927db405836c9c99eef60a0988e2f87b
tomas <tomas@debian.tomas>
2026-06-26 19:54:41 -0300

    add: Se agrego el value object OtherPolicies

---
+++ api/src/domain/value-objects/OtherPolicies.ts
@@ -0,0 +1,15 @@
+export class OtherPolicies {
+  constructor(
+    public quietHoursFrom: string,
+    public quietHoursUntil: string,
+    public hasSmookingAreas: boolean,
+    public allowExternalGuest: boolean,
+    public allowPets: boolean
+  ) {
+    this.quietHoursFrom = quietHoursFrom;
+    this.quietHoursUntil = quietHoursUntil;
+    this.hasSmookingAreas = hasSmookingAreas;
+    this.allowExternalGuest = allowExternalGuest;
+    this.allowPets = allowPets;
+  }
+}
