2de3a963131231b312f66efefc1097bc743d0e8f
tomas <tomas@debian.tomas>
2026-06-26 16:56:34 -0400

    add: se agrego el atributo status a User

--- api/src/domain/entities/User.ts
+++ api/src/domain/entities/User.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
-import { UserDTO, CreateUserDTO } from "../dto/UserDTO";
 
 export type UserRole = "admin" | "manager" | "employee";
+export type Status = "active" | "blocked" | "pending"
 
 export class User {
   public readonly id: number | null;
@@ -10,24 +10,26 @@ export class User {
   public lastName: string | null;
   public role: UserRole;
   public isEmailVerified: boolean;
-  public avatar:string;
+  public avatar: string;
+  private status: Status;
   public createdAt: Date;
   public updatedAt: Date;
-  private passwordHash:string;
-  public lastResendEmail:Date;
+  private passwordHash: string;
+  public lastResendEmail: Date;
   private waitingPeriod: number = 5 * 60 * 1000;
 
   constructor(
-    id:number | null, 
-    username:string, 
-    firstName:string, 
-    lastName:string | null,
+    id: number | null,
+    username: string,
+    firstName: string,
+    lastName: string | null,
     role: UserRole,
-    passwordHash:string, 
-    isEmailVerified:boolean, 
-    lastResendEmail:Date, 
-    avatar:string, 
-    createdAt:Date, 
+    passwordHash: string,
+    isEmailVerified: boolean,
+    lastResendEmail: Date,
+    avatar: string,
+    status: Status,
+    createdAt: Date,
     updatedAt: Date
   ) {
     this.id = id;
@@ -39,43 +41,45 @@ export class User {
     this.isEmailVerified = isEmailVerified;
     this.lastResendEmail = lastResendEmail;
     this.avatar = avatar;
+    this.status = status;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
 
   }
 
   // Metodos de clase
-   static async passwordHash(password:string, saltRounds = 10): Promise<string> {
+  static async passwordHash(password: string, saltRounds = 10): Promise<string> {
     try {
       return await bcrypt.hash(password, saltRounds);
-    } catch(e) {
+    } catch (e) {
       throw new Error("Error hashing password");
     }
   }
 
-  static async newUser(username:string, firstName:string, role:UserRole, password:string ) : Promise<User> {
+  static async newUser(username: string, firstName: string, role: UserRole, password: string): Promise<User> {
     const hashPassword = await User.passwordHash(password);
     const isEmailVerified = false;
     const lastResendEmail = new Date();
     const defaultAvatar = "url/to/default/avatar";
+    const status = "pending";
     const createdAt = new Date();
     const updatedAt = new Date();
 
 
-    return new User(null, username, firstName, null, role, hashPassword, isEmailVerified, lastResendEmail, defaultAvatar, createdAt, updatedAt);
+    return new User(null, username, firstName, null, role, hashPassword, isEmailVerified, lastResendEmail, defaultAvatar, status, createdAt, updatedAt);
   }
 
   // Metodos de instancia
-  async comparePassword(password:string): Promise<boolean> {
+  async comparePassword(password: string): Promise<boolean> {
     try {
       return await bcrypt.compare(password, this.passwordHash);
-    }catch(e){
+    } catch (e) {
       return false
     }
   };
 
-  async changePassword(newPassword:string, repeatPassword:string): Promise<void> {
-    if(newPassword != repeatPassword) {
+  async changePassword(newPassword: string, repeatPassword: string): Promise<void> {
+    if (newPassword != repeatPassword) {
       throw new Error("Passwords doesnt match");
     }
 
@@ -84,7 +88,7 @@ export class User {
   }
 
   // Getters and Setters
-  setLastResenEmail():void {
+  setLastResenEmail(): void {
     this.lastResendEmail = new Date()
   };
 
