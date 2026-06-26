078fefd0166d8e52e7f52e39d88d8cfc50873212
tomas <tomas@debian.tomas>
2026-06-26 10:46:04 -0300

    add: Se creo UserDTO

---
+++ api/src/domain/dto/UserDTO.ts
@@ -0,0 +1,21 @@
+export interface UserDTO {
+  id?: number;
+  username: string;
+  firstname: string;
+  lastname?: string;
+  hashPassword: string;
+  isValidEmail: boolean;
+  lastResendEmail: Date;
+  role: string;
+  avatar: string;
+  createdAt: Date;
+  updateAt: Date;
+}
+
+
+export interface CreateUserDTO {
+  username: string;
+  firstname: string;
+  lastname?: string;
+  password: string;
+}
