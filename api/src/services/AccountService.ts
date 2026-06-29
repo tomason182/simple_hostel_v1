c5d38e7dee7a90adfbd942882c38198ed313c3cb
tomas <tomas@debian.tomas>
2026-06-29 12:18:27 -0300

    add: Se agrego el servicio de crear cuenta

--- api/src/services/AccountService.ts
+++ api/src/services/AccountService.ts
@@ -1,51 +1,64 @@
 import { User } from "../domain/entities/User";
 import { Property } from "../domain/entities/Property";
 import { AccessControl } from "../domain/entities/AccessControl";
-import { IPoliciesRepository } from "../domain/ports/IPoliciesRepository";
 import { IPropertyRepository } from "../domain/ports/IPropertyRepository";
 import { IUserRepository } from "../domain/ports/IUserRepository";
 import { IAccessControlRepository } from "../domain/ports/IAccessControlRepository";
-import { EmailServiceSMTP } from "../infraestructure/email/EmailServiceSMTP";
-import { CreateUserDTO } from "../domain/dto/UserDTO";
-import { CreatePropertyDTO } from "../domain/dto/PropertyDTO";
+import { EmailService } from "../services/EmailService";
+import { IAccountService } from "../domain/interfaces/IAccountService";
 
-export class RegistrationService {
+export class AccountService implements IAccountService {
   constructor(
     private userRepository: IUserRepository,
     private propertyRepository: IPropertyRepository,
-    private accessControl: IAccessControlRepository,
-    private emailService: EmailServiceSMTP
+    private accessControlRepository: IAccessControlRepository,
+    private emailService: EmailService
   ) {
     this.userRepository = userRepository;
     this.propertyRepository = propertyRepository;
-    this.accessControl = accessControl;
+    this.accessControlRepository = accessControlRepository;
     this.emailService = emailService;
   };
 
-  async register(userDTO: CreateUserDTO, propertyDTO: CreatePropertyDTO): Promise<{ status: string, msg: string }> {
+  async createAccount(username: string, password: string, firstname: string, propertyName: string): Promise<{ msg: string }> {
     // 1. Comprobar si el usuario existe.
-    const userExist = await this.userRepository.findByUsername(userDTO.username);
+    const userExist = await this.userRepository.findByUsername(username);
 
     if (userExist !== null) {
       throw new Error("USER_EXIST");
     }
 
     // 2. Crear la entidad User.
-    const user = await User.fromCreateUserDTO(userDTO);
+    const user = await User.createNewUser(username, password, firstname);
 
-    // 3. Guardar el usuario en la base de datos.:while (condition)
+    // 3. Guardar el usuario en la base de datos.
     await this.userRepository.save(user);
 
     // 4. Crear la entidad Property.
-    const property = Property.fromCreatePropertyDTO(propertyDTO)
+    const property = Property.createNewProperty(propertyName);
 
     // 5. Guardar la propiedad en la bd.
     await this.propertyRepository.save(property);
 
+    // 6. Crear la entidad AccessControl.
+    const accessControl = AccessControl.createNewAccessControl(user.getId(), property.getId());
+
+    // 7. Guardar el accessControl.
+    await this.accessControlRepository.save(accessControl);
+
+    // 8. Enviar email para validar cuenta.
+    await this.emailService.validateAccountEmail(user, property, accessControl);
 
     return {
-      status: "ok",
+
       msg: "USER_REGISTER_SUCCESSFULLY"
     }
+  }
+
+  async deleteAccount(username: string, password: string): Promise<{ msg: string; }> {
+
+    // TODO. logica para eliminar cuenta.
+
+    return { msg: "ACCOUNT_DELETED" };
   }
 } 
