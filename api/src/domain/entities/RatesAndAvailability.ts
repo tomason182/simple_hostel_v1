12500cc9f32b4b87ffe65d5de1d6bea63ef0a9d2
tomas <tomas@debian.tomas>
2026-06-30 17:28:59 -0400

    add: Se creo la entidad Rates & availabiliy

---
+++ api/src/domain/entities/RatesAndAvailability.ts
@@ -0,0 +1,22 @@
+export class RatesaAndAvailability {
+  constructor(
+    public id: number | null,
+    public propertyId: number,
+    public roomTypeId: number,
+    public date: Date,
+    public customRate: number,
+    public roomsToSell: number,
+    public createdBy: number,
+    public createdAt: Date
+  ) {
+    this.id = id;
+    this.propertyId = propertyId;
+    this.roomTypeId = roomTypeId;
+    this.date = date;
+    this.customRate = customRate;
+    this.roomsToSell = roomsToSell;
+    this.createdBy = createdBy;
+    this.createdAt = createdAt;
+
+  }
+}
