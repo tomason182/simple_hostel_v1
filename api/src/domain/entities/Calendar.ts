c67d4d61766ecf2a483089d1c68d6ae41eddda84
tomas <tomas@debian.tomas>
2026-07-03 00:29:43 -0300

    add: se creo metodo para comprobar disponibilidad

--- api/src/domain/entities/Calendar.ts
+++ api/src/domain/entities/Calendar.ts
@@ -3,20 +3,21 @@ import { RatesAndAvailability } from "./RatesAndAvailability";
 import { Reservation } from "./Reservation";
 import { RoomType } from "./RoomTypes";
 
-class Calendar {
+export class Calendar {
 
   private roomTypes = new Map<number, Map<number, CalendarDay>>();
 
-  static build(rates: Array<RatesAndAvailability>, rooms: Array<RoomType>, reservations: Array<Reservation>, from: Date, to: Date) {
+  static build(rates: Array<RatesAndAvailability>, reservations: Array<Reservation>) {
     const calendar = new Calendar();
     // 1. Iterar sobre cada tarifa
     for (const rate of rates) {
       let roomRates = calendar.roomTypes.get(rate.roomTypeId);
       const timestamp = rate.date.getTime();
+      const calendarDay = CalendarDay.build(rate, reservations);
 
       if (!roomRates) {
         roomRates = new Map<number, CalendarDay>();
-        roomRates.set(timestamp, new CalendarDay(rate, []))
+        roomRates.set(timestamp, calendarDay);
 
         calendar.roomTypes.set(rate.roomTypeId, roomRates);
         continue;
@@ -26,39 +27,68 @@ class Calendar {
         throw new Error("DUPLICATED_RATE_CONFIG");
       }
 
-      roomRates.set(timestamp, new CalendarDay(rate, []));
+      roomRates.set(timestamp, calendarDay);
     };
 
-    for (const reservation of reservations) {
-      const selectedRooms = reservation.selectedRooms;
-      for (const selectedRoom of selectedRooms) {
-        const roomId = selectedRoom.roomTypeId;
-        const roomCalendar = calendar.roomTypes.get(roomId);
 
-        if (!roomCalendar) {
-          throw new Error("MISSING_ROOM_TYPE");
-        }
-
-        for (const [timestamp, day] of roomCalendar.entries()) {
-          if (reservation.isActiveOn(new Date(timestamp))) {
-            day.addReservation(reservation)
-          }
-        }
-      }
-    }
 
     return calendar
+  }
+
+  public hasAvailability(roomTypeId: number, qty: number, date: Date) {
+    const calendarDayMap = this.roomTypes.get(roomTypeId);
+    if (!calendarDayMap) {
+      throw new Error("NO_RATES_SET");
+    }
+    const calendarDay = calendarDayMap.get(date.getTime())
+    if (!calendarDay) {
+      throw new Error("NO_RATES_SET");
+    }
+
+    const reservedQty = calendarDay.reservedQty
+
+    if (calendarDay.rate.roomsToSell - reservedQty < qty) {
+      return false;
+    }
+
+    return true
+
   }
 }
 
 class CalendarDay {
 
   constructor(
-    private rate: RatesAndAvailability,
-    private reservations: Array<Reservation>
+    public rate: RatesAndAvailability,
+    private reservations: Array<Reservation>,
+    public reservedQty: number = 0
   ) {
     this.rate = rate;
-    this.reservations = reservations
+    this.reservations = reservations;
+  }
+
+  static build(rate: RatesAndAvailability, reservations: Array<Reservation>): CalendarDay {
+    const date = rate.date;
+    const roomTypeId = rate.roomTypeId;
+    const calendarDay = new CalendarDay(rate, []);
+
+    for (const reservation of reservations) {
+      if (!reservation.isActiveOn(date)) {
+        continue;
+      }
+
+      if (reservation.getQuantity(roomTypeId) <= 0) {
+        continue;
+      }
+
+      calendarDay.reservedQty += reservation.getQuantity(roomTypeId);
+
+      calendarDay.reservations.push(reservation);
+
+    }
+
+    return calendarDay;
+
   }
 
   addReservation(reservation: Reservation) {
