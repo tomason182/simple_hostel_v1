453cc19b1abd97f380a4721e3174ab70fa9013b7
tomas <tomas@debian.tomas>
2026-07-03 23:34:36 -0300

    add: Se creo Room

--- api/src/domain/entities/Room.ts
+++ api/src/domain/entities/Room.ts
@@ -1,31 +1,28 @@
 import { Bed } from "./Bed";
-import { RoomType } from "./RoomTypes";
 
 export class Room {
-  private beds: Array<Bed> = [];
-
   constructor(
-    public id: number | null,
-    public roomType: RoomType,
-    public name: string,
+    private id: number | null,
+    private name: string,
+    private beds: Array<Bed>,
   ) {
     this.id = id;
-    this.roomType = roomType;
     this.name = name;
-  }
+    this.beds = beds;
+  };
 
-  static make(roomType: RoomType, name: string) {
-    return new Room(null, roomType, name)
-  }
 
-  public addBeds(maxOccupancy: number) {
-    if (!this.id) {
-      throw new Error("NULL_ID. Save the room first before adding beds");
-    }
+  static make(name: string, maxOccupancy: number) {
+    const beds: Array<Bed> = [];
     for (let i = 0; i < maxOccupancy; i++) {
-      const bed = Bed.make(i + 1, this.id);
-      this.beds.push(bed);
-    }
+      beds.push(Bed.make(i + 1))
+    };
+
+    return new Room(
+      null,
+      name,
+      beds
+    )
 
   }
 }
