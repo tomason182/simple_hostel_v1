3a85b65aa9f742d2b26e68ab3610a2af55290915
tomas <tomas@debian.tomas>
2026-07-07 18:56:54 -0300

    add: Se agregaron la camas otorgadas en selectedRoom

--- api/src/domain/value-objects/SelectedRoom.ts
+++ api/src/domain/value-objects/SelectedRoom.ts
@@ -1,4 +1,28 @@
-export type SelectedRoom = {
-  roomTypeId: number;
-  quantity: number
+import { Bed } from "../entities/Bed";
+
+export class SelectedRoom {
+  constructor(
+    private roomTypeId: number,
+    private quantity: number,
+    private beds: Array<Bed>,
+  ) {
+    this.roomTypeId = roomTypeId;
+    this.quantity = quantity;
+    this.beds = beds
+  }
+
+  public getRoomTypeId(): number {
+    return this.roomTypeId;
+  }
+
+  public getQuantity(): number {
+    return this.quantity;
+  }
+
+  public getBeds(): Array<Bed> {
+    return this.beds;
+  }
+
+
+
 }
