6b3d2aa71122b94c7f84e62460d6b85dc35b31fd
tomas <tomas@debian.tomas>
2026-07-08 20:58:19 -0300

    add: Se agrego nueva version de Calendar

--- api/src/domain/entities/Calendar.ts
+++ api/src/domain/entities/Calendar.ts
@@ -2,9 +2,9 @@ import { RatesAndAvailability } from "./RatesAndAvailability";
 import { Reservation } from "./Reservation";
 
 export class Calendar {
-  // roomTypeId --> BedId --> timestamp --> rate | reservation
-  private roomTypes = new Map<number, BedTimeline>();
-  private rates = new Map<number, Map<number, RatesAndAvailability>>();
+  // roomTypeId --> timestamp -->  CalendarDay (Rates | bed --> reservation)
+  private roomTypes = new Map<number, Map<number, CalendarDay>>();
+  private _rates = new Map<number, Map<number, RatesAndAvailability>>();
 
   private static buildRateIndex(rates: Array<RatesAndAvailability>): Map<number, Map<number, RatesAndAvailability>> {
     const index = new Map<number, Map<number, RatesAndAvailability>>();
@@ -31,7 +31,7 @@ export class Calendar {
   }
 
   public getRate(roomTypeId: number, timestamp: number): RatesAndAvailability {
-    const timeline = this.rates.get(roomTypeId);
+    const timeline = this._rates.get(roomTypeId);
 
     if (!timeline) {
       throw new Error("ROOM_TYPE_NOT_FOUND");
@@ -46,16 +46,11 @@ export class Calendar {
     return rate;
   }
 
-  public nextDay(date: number): number {
-    const next = new Date(date);
-    next.setDate(next.getDate() + 1);
-    return next.getTime()
-  }
 
   static build(reservations: Array<Reservation>, rates: Array<RatesAndAvailability>): Calendar {
     const calendar = new Calendar();
 
-    calendar.rates = Calendar.buildRateIndex(rates);
+    calendar._rates = Calendar.buildRateIndex(rates);
 
     for (const reservation of reservations) {
       // 1. Obtener los cuartos seleccionados en la reserva --> { roomTypeId, quantity, beds[]}
@@ -65,70 +60,60 @@ export class Calendar {
       for (const selectedRoom of selectedRooms) {
         const roomTypeId = selectedRoom.getRoomTypeId();
         const beds = selectedRoom.getBeds();
-
         // 3. Iteramos sobre cada una de las camas que devuelve selectedRoom.
         // NOTA. Estas camas pertenecen a un tipo de cuarto. Si el listado esta vacio no se produce la iteracion.
-        for (const bed of beds) {
-          const bedId = bed.getId();
-          // 4 Buscar en calendar.roomTypes si el roomTypeId ya fue agregado.
-          let bedTimeline = calendar.roomTypes.get(roomTypeId);
 
-          if (!bedTimeline) {
-            // 5. Si no fue agregado, se crea un bedTimeline nuevo y se lo agrega junto con roomTypeId a roomTypes.
-            bedTimeline = new BedTimeline();
-            calendar.roomTypes.set(roomTypeId, bedTimeline)
+        let calendarTimeline = calendar.roomTypes.get(roomTypeId);
+        if (!calendarTimeline) {
+          calendarTimeline = new Map();
+          calendar.roomTypes.set(roomTypeId, calendarTimeline);
+        }
+        for (let date = reservation.checkIn.getTime(); date < reservation.checkOut.getTime(); date = calendar.nextDay(date)) {
+          const rate = calendar.getRate(roomTypeId, date);
 
+          let calendarDay = calendarTimeline.get(date);
+
+          if (!calendarDay) {
+            calendarDay = new CalendarDay(rate);
+            calendarTimeline.set(date, calendarDay);
           }
-          // 6. Para cada dia de estadia de la reserva se optiene el cuadro tarifario "rate".
-          for (let date = reservation.checkIn.getTime(); date < reservation.checkOut.getTime(); date = calendar.nextDay(date)) {
-            const rate = calendar.getRate(roomTypeId, date);
-            bedTimeline.add(reservation, rate, bedId, new Date(date));
+
+          for (const bed of beds) {
+            const bedId = bed.getId();
+            // 4 Buscar en calendar.roomTypes si el roomTypeId ya fue agregado.
+            calendarDay.addBed(bedId, reservation)
           }
         }
       }
     }
     return calendar;
   }
+
+
+  public nextDay(date: number): number {
+    const next = new Date(date);
+    next.setDate(next.getDate() + 1);
+    return next.getTime()
+  }
 }
 
-class BedTimeline {
-  // bedId --> timestamps --> BedOccupancy
-  private beds = new Map<number, DayTimeline>();
+class CalendarDay {
+  private readonly beds = new Map<number, Reservation>
 
+  constructor(
+    private readonly rate: RatesAndAvailability,
+  ) {
+    this.rate = rate;
+  }
 
-  public add(reservation: Reservation, rate: RatesAndAvailability, bedId: number, date: Date) {
-    const timestamp = date.getTime();
-
-    let dayTimeline = this.beds.get(bedId);
-
-    if (!dayTimeline) {
-      dayTimeline = new DayTimeline();
-      this.beds.set(bedId, dayTimeline)
-    }
-
-    if (dayTimeline.has(timestamp)) {
+  public addBed(bedId: number, reservation: Reservation) {
+    if (this.beds.has(bedId)) {
       throw new Error("DOUBLE_ASSIGNMENT");
     }
-
-    dayTimeline?.add(reservation, rate, timestamp);
-
+    this.beds.set(bedId, reservation)
   }
 }
 
-class DayTimeline {
-  private day = new Map<number, BedOccupancy>;
-
-  public add(reservation: Reservation, rate: RatesAndAvailability, timestamp: number) {
-
-    const bedOccupancy = new BedOccupancy(reservation, rate);
-    this.day.set(timestamp, bedOccupancy);
-
-  }
-
-  public has(timestamp: number) {
-    return this.day.has(timestamp);
-  }
-}
 
 class BedOccupancy {
   constructor(
