c8e1b5785e54ccd799218b1aaab6002e6001cddb
tomas <tomas@debian.tomas>
2026-07-14 16:25:15 -0400

    add: Se agrego el repositorio de access control

---
+++ api/src/infraestructure/repositories/AccessControlRepository.ts
@@ -0,0 +1,35 @@
+import { AccessControl } from "../../domain/entities/AccessControl";
+import { IAccessControlRepository } from "../../domain/ports/IAccessControlRepository";
+import { UnitOfWork } from "../transactions/UnitOfWork";
+
+export class AccessControlRepository implements IAccessControlRepository {
+  constructor(private readonly uow: UnitOfWork) {
+    this.uow = uow;
+  }
+
+  async save(accessControl: AccessControl): Promise<AccessControl> {
+    const query = `INSERT INTO access_control (
+                      user_id, 
+                      property_id, 
+                      role, 
+                      created_at, 
+                      updated_at
+                    ) VALUES ($1, $2, $3, $4, $5); RETURNING id;`;
+
+    const result = await this.uow.query(query, [
+      accessControl.userId,
+      accessControl.propertyId,
+      accessControl.role,
+      accessControl.createdAt,
+      accessControl.updatedAt]
+    );
+
+    const id = result.rows[0].id;
+
+    accessControl.setId(id);
+
+    return accessControl;
+  }
+
+
+}
