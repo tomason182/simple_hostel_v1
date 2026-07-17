919074f389a02b13c4386f4d3c3bd0087560dba9
tomas <tomas@debian.tomas>
2026-07-17 20:16:48 -0300

    add: se permite null como valor de currency

--- api/src/domain/value-objects/Currencies.ts
+++ api/src/domain/value-objects/Currencies.ts
@@ -1,24 +1,18 @@
 export class Currencies {
   constructor(
-    public baseCurrency: string,
-    public paymentCurrency: string
+    public baseCurrency: string | null,
+    public paymentCurrency: string | null,
   ) {
     this.baseCurrency = baseCurrency;
     this.paymentCurrency = paymentCurrency;
   };
 
 
-  public getPaymentCurrency(): string {
-    if (!this.paymentCurrency) {
-      throw new Error("NO_PAYMENT_CURRENCY_SET");
-    }
+  public getPaymentCurrency(): string | null {
     return this.paymentCurrency;
   }
 
-  public getBaseCurrency(): string {
-    if (!this.baseCurrency) {
-      throw new Error("NO_BASE_CURRENCY_SET")
-    }
+  public getBaseCurrency(): string | null {
     return this.baseCurrency;
   }
 
