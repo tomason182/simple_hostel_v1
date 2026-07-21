c8d4d60ed9f9fef4895912b51d70db78f1c32b6b
tomas <tomas@debian.tomas>
2026-07-21 14:03:01 -0300

    add: se agrego el metodo me() en authService

--- app/src/services/AuthService.ts
+++ app/src/services/AuthService.ts
@@ -2,11 +2,16 @@ import apiClient from "../api/ApiClient";
 
 import type { LoginInputDTO } from "../dto/auth/LoginInputDTO";
 import type { LoginOutputDTO } from "../dto/auth/LoginInputDTO";
+import type { User } from "../models/User";
 
 
 class AuthService {
   async login(dto: LoginInputDTO): Promise<LoginOutputDTO> {
     return await apiClient.post<LoginInputDTO, LoginOutputDTO>("/auth/login", dto);
+  }
+
+  async me(): Promise<User> {
+    return apiClient.get<User>("/auth/me")
   }
 }
 
