05b6334f168807c5c10dbd847ddc0c776e4144b9
tomas <tomas@debian.tomas>
2026-07-21 15:08:06 -0300

    add: se instalo y configuro react query

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { AuthProvider } from './context/AuthContext.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './config/queryClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
 
-      <App />
+        <App />
 
-    </AuthProvider>
+      </AuthProvider>
+
+    </QueryClientProvider>
+
   </StrictMode>
 )
