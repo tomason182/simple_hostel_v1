d947dabb36a44015cd4b8feffaf897f0635edc70
tomas <tomas@debian.tomas>
2026-08-08 15:21:38 -0400

    API: se agrego el schema para roomtype

--- api/src/infraestructure/http/schemas/roomTypeSchema.ts
+++ api/src/infraestructure/http/schemas/roomTypeSchema.ts
@@ -1,6 +1,11 @@
-import { ParamSchema } from "express-validator"
+import { ParamSchema } from "express-validator";
+import { Gender, RoomTypeLiteral } from "../../../domain/entities/RoomTypes";
 
-const roomTypeDescriptionValidator: ParamSchema = {
+const roomTypeLiteral: RoomTypeLiteral[] = ["PRIVATE", "DORM"]
+const gender: Gender[] = ["MIXED", "MALE", "FEMALE"];
+
+
+const descriptionValidator: ParamSchema = {
   in: ["body"],
   trim: true,
   isString: {
@@ -10,7 +15,76 @@ const roomTypeDescriptionValidator: ParamSchema = {
   exists: {
     bail: true,
     errorMessage: "Description must be provided"
+  },
+  notEmpty: {
+    bail: true,
+    errorMessage: "Description must be provided"
+  },
+  isLength: {
+    options: {
+      min: 1,
+      max: 255
+    },
+    errorMessage: "RoomType description max lenght is 255 characters"
   }
 }
 
+const typeValidator: ParamSchema = {
+  in: ["body"],
+  trim: true,
+  exists: {
+    bail: true,
+    errorMessage: "Type must be provided"
+  },
+  isIn: {
+    options: [roomTypeLiteral],
+    errorMessage: "Room type must be private or dorm"
+  }
+}
 
+const genderValidator: ParamSchema = {
+  in: ["body"],
+  trim: true,
+  exists: {
+    bail: true,
+    errorMessage: "Gender must be provided"
+  },
+  isIn: {
+    options: [gender],
+    errorMessage: "RoomType gender must be one of mixed, male or female"
+  }
+}
+
+const maxOccupancyValidator: ParamSchema = {
+  in: ["body"],
+  trim: true,
+  exists: {
+    bail: true,
+    errorMessage: "maxOccupancy must be provided"
+  },
+  isInt: {
+    options: [{ min: 1 }],
+    errorMessage: "maxOccupancy must be positive integer"
+  }
+}
+
+const inventoryValidator: ParamSchema = {
+  in: ["body"],
+  trim: true,
+  exists: {
+    bail: true,
+    errorMessage: "Inventory must be provided"
+  },
+  isInt: {
+    options: [{ min: 1 }],
+    errorMessage: "Inventory must be positive integer"
+  }
+}
+
+export const roomTypeSchema = {
+  description: descriptionValidator,
+  type: typeValidator,
+  gender: genderValidator,
+  maxOccupancy: maxOccupancyValidator,
+  inventory: inventoryValidator
+}
